8bf2c931d63452d351a5910cc5a89d71b2cac23d
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-14 10:15:32 +0530

    user details displayed successfully

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:user_management/components/material_button.dart';
-import 'package:user_management/controller/signup_controller.dart';
+import 'package:user_management/controller/login_controller.dart';
 import 'package:user_management/utlities/constant.dart';
+import 'package:user_management/view/edit_view.dart';
 
 class HomePage extends StatelessWidget {
   static String id = "Home_Page";
   const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    context.read<SignUpController>().getAllUserDetails();
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
@@ -21,6 +20,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: ListView(
+        physics: const BouncingScrollPhysics(),
         children: [
           sizedBox20,
           const Center(
@@ -37,33 +37,47 @@ class HomePage extends StatelessWidget {
               width: double.infinity,
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Name    :",
-                      style: textStyle,
-                    ),
-                    sizedBox20,
-                    const Text(
-                      "Email    :",
-                      style: textStyle,
-                    ),
-                    sizedBox20,
-                    const Text(
-                      "Age       :",
-                      style: textStyle,
-                    ),
-                    sizedBox20,
-                    const Text(
-                      "Phone   :",
-                      style: textStyle,
-                    ),
-                    SizedBox(
-                      height: size.height / 5,
-                    ),
-                    MaterialButtonWidget(onClick: () {}, text: "Edit Profile")
-                  ],
+                child: Consumer<LogInController>(
+                  builder: (context, value, child) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Name    :  ${value.loggedUserModel.name}",
+                        style: textStyle,
+                      ),
+                      sizedBox20,
+                      Text(
+                        "Email    :  ${value.loggedUserModel.email}",
+                        style: textStyle,
+                      ),
+                      sizedBox20,
+                      value.loggedUserModel.age == null
+                          ? Visibility(
+                              visible: false,
+                              child: Text(
+                                "Age      :   ${value.loggedUserModel.age}",
+                                style: textStyle,
+                              ),
+                            )
+                          : Text(
+                              "Age       :   ${value.loggedUserModel.age}",
+                              style: textStyle,
+                            ),
+                      sizedBox20,
+                      Text(
+                        "Phone   :  ${value.loggedUserModel.phone}",
+                        style: textStyle,
+                      ),
+                      SizedBox(
+                        height: size.height / 5,
+                      ),
+                      MaterialButtonWidget(
+                          onClick: () {
+                            Navigator.pushNamed(context, EditPage.id);
+                          },
+                          text: "Edit Profile")
+                    ],
+                  ),
                 ),
               ),
             ),
