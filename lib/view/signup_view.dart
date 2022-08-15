f099da679562f91278cbe8fb230962e57e6fb5e7
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-15 14:36:06 +0530

    almost finished

--- lib/view/signup_view.dart
+++ lib/view/signup_view.dart
@@ -4,6 +4,7 @@ import 'package:user_management/components/material_button.dart';
 import 'package:user_management/components/text_form_field.dart';
 import 'package:user_management/controller/signup_controller.dart';
 import 'package:user_management/utlities/constant.dart';
+import 'package:user_management/view/login_view.dart';
 
 class SignUpPage extends StatelessWidget {
   static String id = "signUp-view";
@@ -23,39 +24,67 @@ class SignUpPage extends StatelessWidget {
             horizontal: 15,
           ),
           child: ListView(
+            physics: const BouncingScrollPhysics(),
             children: [
-              const Center(
+              Center(
                 child: Text(
-                  "Register",
-                  style: textStyle,
+                  "Create New Account",
+                  style: textStyle.copyWith(fontSize: 25),
                 ),
               ),
-              sizedBox20,
+
+              sizedBoxHeight50,
               TextFormFieldWidget(
-                text: "Enter name",
+                text: "Enter your name",
                 controller: registerController.nameController,
               ),
-              sizedBox10,
+              sizedBoxHeight20,
               TextFormFieldWidget(
-                  text: "Enter Email",
+                  text: "Enter your Email",
                   controller: registerController.emailController),
-              sizedBox10,
+              sizedBoxHeight20,
               TextFormFieldWidget(
-                text: "Enter phone",
+                text: "Enter your phone",
                 controller: registerController.phoneNumberController,
               ),
-              sizedBox10,
+              sizedBoxHeight20,
               TextFormFieldWidget(
                   text: "Enter password",
                   controller: registerController.passwordController),
-              sizedBox10,
+              sizedBoxHeight20,
               // const TextFormFieldWidget(text: " Re-enter Password"),
               // sizedBox20,
               MaterialButtonWidget(
-                  onClick: () {
-                    registerController.registerUser(context);
-                  },
-                  text: "Signup")
+                onClick: () async {
+                  final errorMessage =
+                      await registerController.registerUser(context);
+                  // ignore: use_build_context_synchronously
+                  registerController.checkFormField(context, errorMessage);
+                },
+                text: "Signup",
+              ),
+              sizedBoxHeight50,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account?",
+                    style: TextStyle(color: whiteColor),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        LoginPage.id,
+                      );
+                    },
+                    child: Text(
+                      "LogIn",
+                      style: textStyle.copyWith(fontSize: 18),
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
